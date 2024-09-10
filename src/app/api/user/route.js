0f9941acf45afafc8d5f16a3f55c360f98403f35
kaustubhraut-07 <kaustubh.raut@fxis.ai>
2024-09-10 15:06:35 +0530

    backedn routes

---
+++ src/app/api/user/route.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+export function GET(request){
+    const users = [
+        {
+            id: 1,
+            name: "Kaustubh"
+
+        },
+        {
+            id: 2,
+            name: "ankit"
+
+        },
+        {
+            id: 3,
+            name: "yogesh"
+
+        },
+    ];
+    return NextResponse.json(users);
+
+}
